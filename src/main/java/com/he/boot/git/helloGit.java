694e1b72d178c41e701043249970d6b751b82125
he <he@test.com>
2022-11-02 18:08:36 +0800

    test commit1

--- src/main/java/com/he/boot/git/helloGit.java
+++ src/main/java/com/he/boot/git/helloGit.java
@@ -5,5 +5,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git2 test");
     }
 }
