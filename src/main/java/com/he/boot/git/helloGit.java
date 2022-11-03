f3badee0727844efa1cbf648fd8acf45ee41fa32
xiaoke27s <117259894+xiaoke27s@users.noreply.github.com>
2022-11-03 15:24:42 +0800

    pull commit

--- src/main/java/com/he/boot/git/helloGit.java
+++ src/main/java/com/he/boot/git/helloGit.java
@@ -8,6 +8,7 @@ public class helloGit {
         System.out.println("hello git2 test");
         System.out.println("hello git2 test new");
         System.out.println("hello git2 test commit2");
-        System.out.println("hello gitHub test1");
+        System.out.println("hello gitHub test1");S
+        System.out.println("pull gitHub test1");
     }
 }
